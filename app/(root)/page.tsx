04514b6044e5381dcff75a052ee002801c046e0d
Kelvin Ebima <kelvinebima@gmail.com>
2024-07-09 14:10:44 -0700

    implement Total balance box, left and right sidebar, and mobile nav bar.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,40 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedin = {firstName: 'Kelvin', lastName: 'Ebima', email: 'kelvin@mail.com' };
+
   return (
-    <div>
-      Home
+    <div className="home">
+      <div className="home-content">
+        <header className='home-header'>
+          <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedin?.fiirstName || 'Guest'}
+          subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.75}
+          />
+
+          
+        </header>
+
+        RECENT TRANSACTIONS
+
+      </div>
+
+      <RightSideBar
+       user= {loggedin}
+       transactions={[]}
+       banks={[]}
+       />
     </div>
   )
 }
